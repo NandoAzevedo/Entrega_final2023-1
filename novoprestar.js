e821648d1e30ebc1014ec1db142e4ce98d326d5c
Jean Carlos Liang Tan <jeanliangtan@hotmail.com>
2023-07-03 02:46:28 -0300

    Java script salvando os dados do prestar servico e algumas alteracoes

--- novoprestar.js
+++ novoprestar.js
@@ -1,21 +1,26 @@
 const barraPesquisa = document.getElementById('barra-pesquisa');
 const botoesTrabalho = document.getElementsByClassName('trabalho');
 
-barraPesquisa.addEventListener('input', function() {
-  const termoPesquisa = barraPesquisa.value.toLowerCase();
+if (barraPesquisa) {
+  barraPesquisa.addEventListener('input', function() {
+    const termoPesquisa = barraPesquisa.value.toLowerCase();
 
-  // Percorre os botões de trabalho
-  for (let i = 0; i < botoesTrabalho.length; i++) {
-    const nomeBotao = botoesTrabalho[i].textContent.toLowerCase();
-    
-    // Verifica se o nome do botão contém o termo de pesquisa
-    if (nomeBotao.includes(termoPesquisa)) {
-      botoesTrabalho[i].style.display = 'block';  // Exibe o botão
-    } else {
-      botoesTrabalho[i].style.display = 'none';   // Oculta o botão
+    // Percorre os botões de trabalho
+    for (let i = 0; i < botoesTrabalho.length; i++) {
+      const nomeBotao = botoesTrabalho[i].textContent.toLowerCase();
+
+      // Verifica se o nome do botão contém o termo de pesquisa
+      if (nomeBotao.includes(termoPesquisa)) {
+        botoesTrabalho[i].style.display = 'block';  // Exibe o botão
+      } else {
+        botoesTrabalho[i].style.display = 'none';   // Oculta o botão
+      }
     }
-  }
-});
+  });
+} else {
+  console.log("Elemento 'barra-pesquisa' não encontrado.");
+}
+
 const imagemInput = document.getElementById('imagem-input');
 const imagemPreview = document.getElementById('imagem-preview');
 
@@ -34,3 +39,111 @@ imagemInput.addEventListener('change', function(event) {
   }
 });
 
+// Função para definir o botão selecionado e salvar no Local Storage
+function selecionarBotao(id) {
+  // Obter todos os botões com a classe "trabalho"
+  const botoes = document.getElementsByClassName("trabalho");
+
+  // Remover a classe "selecionado" de todos os botões
+  for (let i = 0; i < botoes.length; i++) {
+    botoes[i].classList.remove("selecionado");
+  }
+
+  // Adicionar a classe "selecionado" ao botão clicado
+  const botaoSelecionado = document.getElementById(id);
+  botaoSelecionado.classList.add("selecionado");
+
+  // Salvar a escolha no Local Storage
+  localStorage.setItem("escolha", id);
+}
+
+// Adicionar o evento de clique aos botões
+const botoes = document.getElementsByClassName("trabalho");
+for (let i = 0; i < botoes.length; i++) {
+  const botao = botoes[i];
+  botao.addEventListener("click", function() {
+    selecionarBotao(botao.id);
+  });
+}
+
+// Verificar se há uma escolha salva no Local Storage e marcar o botão selecionado
+const escolhaSalva = localStorage.getItem("escolha");
+if (escolhaSalva) {
+  const botaoSelecionado = document.getElementById(escolhaSalva);
+  if (botaoSelecionado) {
+    botaoSelecionado.classList.add("selecionado");
+  }
+}
+
+// Função para salvar a escolha e o conteúdo do textarea
+function salvarEscolha() {
+  // Obter a escolha selecionada
+  const escolha = localStorage.getItem("escolha");
+
+  // Obter o conteúdo do textarea
+  const descricao = document.getElementById("descricao").value;
+
+  // Verificar se há uma escolha e conteúdo do textarea
+  if (escolha && descricao) {
+    // Salvar a escolha no Local Storage
+    localStorage.setItem("escolhaSalva", escolha);
+
+    // Salvar o conteúdo do textarea no Local Storage
+    localStorage.setItem("descricaoSalva", descricao);
+
+    // Exibir o alerta com a escolha e conteúdo do textarea
+    alert("Escolha salva: " + escolha + "\nDescrição: " + descricao);
+  } else {
+    // Caso não haja escolha ou conteúdo do textarea
+    alert("Preencha todos os campos.");
+  }
+}
+
+// Obter os checkboxes dos dias da semana
+const checkboxes = document.querySelectorAll('input[name="dias-semana"]');
+
+// Função para salvar os dias selecionados no Local Storage
+function salvarDiasSelecionados() {
+  const diasSelecionados = [];
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      diasSelecionados.push(checkboxes[i].value);
+    }
+  }
+
+  // Salvar os dias selecionados no Local Storage
+  localStorage.setItem("diasSelecionados", JSON.stringify(diasSelecionados));
+
+  console.log("Dias selecionados salvos:", diasSelecionados);
+}
+
+// Adicionar o evento de clique ao botão "salvado"
+const botaoSalvar = document.getElementById("salvado");
+botaoSalvar.addEventListener("click", salvarDiasSelecionados);
+
+// Função para salvar o número de contato no Local Storage
+function salvarNumeroContato() {
+  const numeroContato = document.getElementById("numero-contato-input").value;
+
+  // Verificar se o número de contato foi fornecido
+  if (numeroContato) {
+    // Salvar o número de contato no Local Storage
+    localStorage.setItem("numeroContato", numeroContato);
+
+    console.log("Número de contato salvo:", numeroContato);
+    
+    // Exibir o alerta "Cadastrado com sucesso!"
+    alert("Cadastrado com sucesso!");
+
+    // Redirecionar para a página "tela.html"
+    window.location.href = "tela.html";
+  } else {
+    console.log("Número de contato não fornecido.");
+    alert("Preencha todos os campos.");
+  }
+}
+
+// Adicionar o evento de clique ao botão "Cadastrar"
+const botaoCadastrar = document.getElementById("salvado");
+botaoCadastrar.addEventListener("click", salvarNumeroContato);
--- tela.js
+++ tela.js
@@ -24,3 +24,42 @@ const button = document.getElementById('Acessar');
 button.addEventListener('click', () => {
   window.location.href = 'perfilnovo.html';
 });
+document.addEventListener('DOMContentLoaded', function() {
+  var buttons = document.querySelectorAll('.trabalho');
+  var categories = document.querySelectorAll('.categoria-servico');
+  var activeCategory = null;
+
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      var categoryId = this.id.replace('btn-', 'div-');
+      var categoryToShow = document.getElementById(categoryId);
+
+      if (activeCategory && activeCategory !== categoryToShow) {
+        activeCategory.style.display = 'none';
+        activeCategory.classList.remove('nova-div-estilo');
+        activeCategory = null;
+      }
+
+      if (categoryToShow && activeCategory !== categoryToShow) {
+        categoryToShow.style.display = 'block';
+        categoryToShow.classList.add('nova-div-estilo');
+        activeCategory = categoryToShow;
+      } else {
+        categoryToShow.style.display = 'none';
+        categoryToShow.classList.remove('nova-div-estilo');
+        activeCategory = null;
+      }
+
+      var opcoesServico = document.getElementById('opcoes-servico');
+      if (categoryToShow && opcoesServico.nextSibling !== categoryToShow) {
+        opcoesServico.insertAdjacentElement('afterend', categoryToShow);
+      }
+
+      // Adicionar o conteúdo HTML correspondente à div
+      var divContent = document.getElementById('div-' + this.id.replace('btn-', ''));
+      if (divContent) {
+        categoryToShow.innerHTML = divContent.innerHTML;
+      }
+    });
+  });
+});
